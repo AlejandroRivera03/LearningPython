57714bf23ab08c4eb2c169e3a2cc054e5a267f02
Alejandro <alejandro.rivera0309@gmail.com>
2019-12-13 23:54:07 -0600

    Relational and logic operators

---
+++ 004RelationalAndLogicOperator.py
@@ -0,0 +1,13 @@
+print( 'Operadores Relacionales\n' )
+print( 'Igual que "=="' )
+print( 'Diferente de "!="' )
+print( 'Mayor que ">"' )
+print( 'Mayor o igual que ">="' )
+print( 'Menor que "<"' )
+print( 'Menor o igual que "<="' )
+print( '\n' )
+
+print( 'Operadores Logicos\n' )
+print( 'Negacion "not"' )
+print( 'Conjuncion "and"' )
+print( 'Disyuncion "or"' )
