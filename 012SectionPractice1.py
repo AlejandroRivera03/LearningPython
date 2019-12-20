710e1b820b9c3211649a2ec9b684eca48f365974
Alejandro <alejandro.rivera0309@gmail.com>
2019-12-20 00:38:43 -0600

    Section 7 practice

---
+++ 012SectionPractice1.py
@@ -0,0 +1,6 @@
+print( '{:>20}'.format('Hola Mundo') ) # right alignment with 20 chars
+print( '{:.3}'.format('Hola Mundo') ) # Trucate at fourth char
+print( '{:^20.1}'.format('Hola Mundo') ) # center alignment with 20 chars and trucate at second char
+print( '{:05d}'.format(150) ) # 5 digits filling with zeros (0)
+print( '{:7d}'.format(7887) ) # 7 digits filling with spaces ( )
+print( '{:07.3f}'.format(20.02) ) # 3 ints and 3 decimals
---
+++ 012SectionPractice3.py
@@ -0,0 +1,16 @@
+import sys
+
+if len(sys.argv) == 2:
+    numero = int( sys.argv[1] )
+    if numero < 0 or numero > 9999:
+        print( 'Error - Numero incorrecto' )
+        print( 'python 012SectionPractice3.py [0 - 9999]' )
+    else:
+        cadena = str( numero )
+        longitud = len( cadena )
+
+        for i in range( longitud ):
+            print( '{0:04d}'.format( ( int( cadena[longitud-1-i] ) * 10**i ) ) )
+else:
+    print( 'Error - Argumentos incorrectos' )
+    print( 'python 012SectionPractice3.py [0 - 9999]' )
