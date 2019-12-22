fc402d5e86c621579652e4ed64ca2e1e59ff70c2
Alejandro <alejandro.rivera0309@gmail.com>
2019-12-21 23:58:33 -0600

    Arguments and parameters, Arguments per value and reference and indetermine arguments

--- 013Functions.py
+++ 013Functions.py
@@ -24,3 +24,51 @@ def resta(a=None, b=None): # Here are parameters
 print( resta(5, 2) ) # Here are arguments
 print( resta(b=2, a=5) )
 print( resta() )
+
+# value and reference
+# colecciones, listas, diccionarios, conjuntos are normally passed by reference
+# more simple data is passed by value
+
+def doblar( num ):
+    return num*2
+
+num = 5
+doblar( num ) # passed by value (create a copy)
+print( num )
+
+num = doblar( num ) # simulating by reference
+print( num )
+
+
+def doblarr( data ):
+    for i,n in enumerate(data):
+        data[i] *= 2
+
+data = [1,2,3,4,5]
+doblarr( data ) # passed by reference
+print( data )
+
+doblar( data[:] ) # sending a copy tu simulate passed by value
+print( data )
+
+def indeterminados_posicion(*args):
+    print( args ) # a tupla
+    for arg in args:
+        print( arg )
+indeterminados_posicion( 5, 'Hola', [1,2,3,4,5] )
+
+def indeterminados_nombre(**kwargs):
+    print(kwargs)
+    for kwarg in kwargs:
+        print( kwarg, ' ', kwargs[kwarg] )
+indeterminados_nombre( n=5, c='Hola', l=[1,2,3,4,5] )
+
+def super_funcion(*args, **kwargs):
+    t = 0
+    for arg in args:
+        t += arg
+    print( 'Sumatoria es', t )
+    for kwarg in kwargs:
+        print( kwarg, ' ', kwargs[kwarg] )
+
+super_funcion( 5,10,15,20,15,nombre='alejandro',edad=20 )
