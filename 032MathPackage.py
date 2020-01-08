f5387453dab463ec985bd7267a3afd4cb296961f
Alejandro <alejandro.rivera0309@gmail.com>
2020-01-07 23:58:04 -0600

    Importing some methods from math package

---
+++ 032MathPackage.py
@@ -0,0 +1,26 @@
+import math
+
+print( 'round( 3.2 ) => {}'.format( round( 3.2 ) ) )
+print( 'round( 3.7 ) => {}'.format( round( 3.7 ) ) )
+print( 'math.floor( 3.9 ) => {}'.format( math.floor( 3.9 ) ) )
+print( 'math.ceil( 3.1 ) => {}'.format( math.ceil( 3.1 ) ) )
+print( 'abs( -16 ) => {}'.format( abs( -16 ) ) )
+
+print( '\n' )
+n = [1,2,3]
+print( 'n => {}'.format( n ) )
+print( 'sum( n ) => {}'.format( sum( n ) ) )
+print( 'math.fsum( n ) => {}'.format( math.fsum( n ) ) )
+
+print( '\n' )
+n = [0.9999999, 1, 2, 3]
+print( 'n => {}'.format( n ) )
+print( 'sum( n ) => {}'.format( sum( n ) ) )
+print( 'math.fsum( n ) => {}'.format( math.fsum( n ) ) )
+
+print( '\n' )
+print( 'math.trunc( 100.123456 ) => {}'.format( math.trunc( 100.123456 ) ) )
+print( 'math.pow( 2, 3 ) => {}'.format( math.pow( 2, 3 ) ) )
+print( 'math.sqrt( 9 ) => {}'.format( math.sqrt( 9 ) ) )
+print( 'math.pi => {}'.format( math.pi ) )
+print( 'math.e => {}'.format( math.e ) )
