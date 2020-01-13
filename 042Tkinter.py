b2eb55625cf9e95bc7ed9865bf8c18dc8e9987c1
Alejandro <alejandro.rivera0309@gmail.com>
2020-01-12 23:26:07 -0600

    Learning GUI with Tkinter, starting how to create a window

---
+++ 042Tkinter.py
@@ -0,0 +1,9 @@
+from tkinter import Tk
+
+root = Tk()
+
+root.title( 'Ventanita' )
+root.resizable( 1, 1 ) # first for x axis, second for y axis
+root.iconbitmap( 'icono.ico' )
+
+root.mainloop() # Always put it at the end
