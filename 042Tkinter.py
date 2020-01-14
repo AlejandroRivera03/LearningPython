1861a436ec69ddfcb988b758e04a3bb4917425a8
Alejandro <alejandro.rivera0309@gmail.com>
2020-01-13 20:04:28 -0600

    Thinker's Labels

--- 042Tkinter.py
+++ 042Tkinter.py
@@ -1,4 +1,4 @@
-from tkinter import Tk, Frame
+from tkinter import Tk, Frame, Label, StringVar, PhotoImage
 
 root = Tk()
 
@@ -14,5 +14,21 @@ frame.config( relief='sunken' )
 # frame.pack( side='bottom', anchor='w' )
 # frame.pack( fill='both', expand=1 )
 frame.pack()
+
+texto = StringVar()
+texto.set( 'Tercer etiqueta!' )
+
+Label( frame, text='Primer etiqueta' ).place(x=0, y=0)
+
+label = Label( frame, text='Segunda etiqueta' )
+label.place(x=0, y=25)
+label.config( bg='green', fg='white', font=('Verdana', 24) )
+
+label2 = Label( frame, text='algo' )
+label2.place(x=100, y=0)
+label2.config( textvariable=texto )
+
+imagen = PhotoImage( file='oso.gif' )
+Label( frame, image=imagen, bd=0 ).place(x=0, y=80)
 
 root.mainloop() # Always put it at the end
