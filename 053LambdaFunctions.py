068bdc86a9c7c28561aa3cbde3cc42285140041b
Alejandro <alejandro.rivera0309@gmail.com>
2020-01-27 20:03:25 -0600

    Lambda functions

---
+++ 053LambdaFunctions.py
@@ -0,0 +1,9 @@
+# Lambda functions
+duplicado = lambda num: num*2
+print( duplicado( 4 ) )
+
+reverseString = lambda cadena: cadena[::-1]
+print( reverseString( 'Hola Mundo' ) )
+
+sumar = lambda x,y: x+y
+print( sumar( 3,7 ) )
